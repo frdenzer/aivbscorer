32d17d0f04896c89bec5d0e76a239e6b063fed4d
Frank Denzer <frank.denzer@ard.de>
2024-07-12 17:32:30 +0200

    rowspacing unified

--- app/src/main/java/com/example/aivbscorer/ui/theme/Shapes.kt
+++ app/src/main/java/com/example/aivbscorer/ui/theme/Shapes.kt
@@ -1,7 +1,12 @@
 package com.example.aivbscorer.ui.theme
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
@@ -9,3 +14,8 @@ val Shapes = Shapes(
     medium = RoundedCornerShape(24.dp),
     large = RoundedCornerShape(40.dp)
 )
+
+@Composable
+fun RowSpacing() {
+    Spacer(modifier = Modifier.height(10.dp))
+}
