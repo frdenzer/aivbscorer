999b3f52ab32376b4ad5b127b96ba782dcf84275
Frank Denzer <frank.denzer@ard.de>
2024-07-14 20:03:55 +0200

    nav host:add preview annotation

--- app/src/main/java/com/example/aivbscorer/AppNavigation.kt
+++ app/src/main/java/com/example/aivbscorer/AppNavigation.kt
@@ -1,11 +1,13 @@
 package com.example.aivbscorer
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.aivbscorer.theme.VolleyballScorerTheme
 
+@Preview
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
