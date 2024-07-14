333e6f3a74f3875031c600f719564968501edad4
Frank Denzer <frank.denzer@ard.de>
2024-07-14 21:40:00 +0200

    reverse causes layout to stay top-red-correct

--- app/src/main/java/com/example/aivbscorer/BigNumbers.kt
+++ app/src/main/java/com/example/aivbscorer/BigNumbers.kt
@@ -1,5 +1,8 @@
 package com.example.aivbscorer
 
+import android.content.res.Configuration
+import android.view.Surface
+import android.view.WindowManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -9,7 +12,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.aivbscorer.components.TeamScoreColumn
 import com.example.aivbscorer.data.BlueTeam
 import com.example.aivbscorer.data.RedTeam
@@ -23,6 +29,13 @@ fun BigNumbersPreview() {
 
 @Composable
 internal fun BigNumbers(modifier: Modifier) {
+    val context = LocalContext.current
+    val configuration = LocalConfiguration.current
+    val windowManager = getSystemService(context, WindowManager::class.java)
+    val rotation = windowManager?.defaultDisplay?.rotation
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+    val isReverseLandscape = isLandscape && (rotation == Surface.ROTATION_270)
+
     val wide = Modifier.fillMaxWidth()
     BoxWithConstraints(modifier = modifier) {
         if (maxWidth > maxHeight) {
@@ -31,18 +44,28 @@ internal fun BigNumbers(modifier: Modifier) {
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween,
             ) {
-                TeamScoreColumn(
-                    RedTeam,
-                    modifier
-                        .background(RedTeam.colorId)
-                        .weight(1f)
-                )
+                if (!isReverseLandscape) {
+                    TeamScoreColumn(
+                        RedTeam,
+                        modifier
+                            .background(RedTeam.colorId)
+                            .weight(1f)
+                    )
+                }
                 TeamScoreColumn(
                     BlueTeam,
                     modifier
                         .background(BlueTeam.colorId)
                         .weight(1f)
                 )
+                if (isReverseLandscape) {
+                    TeamScoreColumn(
+                        RedTeam,
+                        modifier
+                            .background(RedTeam.colorId)
+                            .weight(1f)
+                    )
+                }
             }
             return@BoxWithConstraints
         }
